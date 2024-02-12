771e706c8252d8b38502c286adef46927faf547f
John Guzauckas <75325154+jguzauckas@users.noreply.github.com>
2024-02-12 12:15:47 -0500

    Update Check.java

--- 2. Basics Check/Check.java
+++ 2. Basics Check/Check.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 public class Check {
     public static void main (String[] args){
         ArrayList<Double> doubleList = new ArrayList<Double>();
-        for (int i = 0; i < doubleList.size(); i++) {
+        for (int i = 0; i < 100; i++) {
             doubleList.add(Math.random() * 100);
         }
 
